df75505ea33f1ee2a9f034be2ad66aa9144a1d5d
shodiyevf <shodiyevf@yandex.ru>
2021-08-23 15:54:19 +0500

    asd

--- src/server.js
+++ src/server.js
@@ -5,9 +5,11 @@ const SUBCATEGORIES = require('./modules/subcategories/index.js')
 
 const app = express()
 
+const PORT = process.env.PORT || 4000
+
 app.use(express.json())
 app.use(auth)
 app.use(CATEGORIES)
 app.use(SUBCATEGORIES)
 
-app.listen('4000')
+app.listen(PORT)
